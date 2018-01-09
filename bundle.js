fbd8d9e40b423b3b6fc7b59adc4645d2ff600704
이끼룩 <saram4030@gmail.com>
2018-01-09 18:17:17 +0900

    빌드 환경을 구축하다.
    webpack을 이용한 빌드 환경을 구축했습니다.
    - 수동 빌드: npm run build
    - 실시간 빌드: npm run watch

---
+++ bundle.js
@@ -0,0 +1,253 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+class DomContainer {
+    constructor() {
+        this.appBar = document.querySelector('.app-bar');
+        this.searchButton = document.querySelector('.search-button');
+        this.searchBar = document.querySelector('.search-bar');
+        this.searchField = document.querySelector('#search-field');
+        this.autoCompleteList = document.querySelector('.auto-complete-list');
+    }
+    getHoveredItem() {
+		return document.querySelector('.hover');
+	}
+}
+
+class ACResource {
+    constructor() {
+        this.memo = {};
+        this.memoLog = [];
+        this.memoSize = 100;
+    }
+    getData(url) {
+        return fetch(url)
+        .then(response => response.json()
+            .then(data => data)
+        )
+        .catch((err) => {
+            console.log("error: ", err);
+        })
+    }
+    caching(key, value) {
+        if (this.memo.hasOwnProperty(key)) {
+            return;
+        }
+
+        if (this.memoLog.length > this.memoSize) {
+            const key = this.memoLog.shift();
+            delete this.memo[key];
+        }
+
+        this.memo[key] = value;
+        this.memoLog.push(key);
+    }
+}
+
+class ACResponder {
+    constructor(domContainer, acResource, acRenderer, apiURL) {
+        this.domContainer = domContainer
+        this.acResource = acResource
+        this.acRenderer = acRenderer
+        this.apiURL = apiURL
+
+        this.domContainer.searchField.addEventListener('keydown', this.checkKeyCode.bind(this));
+        this.domContainer.searchField.addEventListener('input', this.changeSearchText.bind(this));
+        this.domContainer.autoCompleteList.addEventListener('mouseover', this.mouseOver.bind(this));
+        this.domContainer.autoCompleteList.addEventListener('click', this.clickedItem.bind(this));
+        this.domContainer.searchButton.addEventListener('click', this.clickedSearchButton.bind(this));
+    }
+    checkKeyCode(e) {
+		switch(e.keyCode){
+			case 38: //ArrowUp
+				this.acRenderer.pressedUpKey()
+				break;
+			case 40: //ArrowDown
+				this.acRenderer.pressedDownKey()
+				break;
+			case 13: //Enter
+				this.acRenderer.pressedEnterKey()
+                break;
+		}
+	}
+    changeSearchText(e) {
+		const keyword = e.target.value;
+		if (this.acResource.memo.hasOwnProperty(keyword)) {
+			this.acRenderer.updateRendering(keyword, this.acResource.memo[keyword]);
+			return;
+		}
+
+		const url = this.apiURL + keyword;
+		this.acResource.getData(url).then((data) => {
+            this.acResource.caching(keyword, data[1]);
+    		this.acRenderer.updateRendering(keyword, this.acResource.memo[keyword]);
+        })
+	}
+    mouseOver(e) {
+		const item = e.target;
+		if(!item || item.nodeName !== 'LI') {
+			return;
+		}
+        this.acRenderer.changeHoveredItem(item)
+	}
+	clickedItem(e) {
+		const item = e.target;
+		if(!item || item.nodeName !== 'LI') {
+			return;
+		}
+		this.acRenderer.putSelectedItemToField(item.dataset.name);
+	}
+	clickedSearchButton(e) {
+		this.acRenderer.launchSearchEvent();
+	}
+}
+
+class ACRenderer {
+    constructor(domContainer) {
+        this.domContainer = domContainer
+    }
+    updateRendering(keyword, autoComplete) {
+		const listDom = this.domContainer.autoCompleteList;
+		if(!autoComplete){
+			listDom.innerHTML = ""
+			return false
+		}
+
+		let listDomHTML = "";
+		autoComplete.forEach((item) => {
+
+			const itemHTML = item[0].replace(keyword, `<span>${keyword}</span>`);
+			const itemDom = `<li data-name='${item[0]}'>${itemHTML}</li>`;
+			listDomHTML += itemDom;
+		});
+
+		listDom.innerHTML = listDomHTML;
+	}
+    launchSearchEvent(keyword) {
+		this.domContainer.autoCompleteList.innerHTML = "";
+		this.domContainer.searchField.value = "";
+	}
+    pressedUpKey() {
+        const currHoveredItem = this.domContainer.getHoveredItem();
+        if(!currHoveredItem) {
+            return;
+        }
+        if(currHoveredItem.previousElementSibling) {
+            currHoveredItem.previousElementSibling.classList.add('hover');
+            currHoveredItem.classList.remove('hover');
+        }
+    }
+    pressedDownKey() {
+        const currHoveredItem = this.domContainer.getHoveredItem();
+        if(!currHoveredItem) {
+            const autoCompleteList = this.domContainer.autoCompleteList;
+            if(autoCompleteList.childNodes) {
+                autoCompleteList.childNodes[0].classList.add('hover')
+            }
+            return;
+        }
+        if(currHoveredItem.nextElementSibling) {
+            currHoveredItem.nextElementSibling.classList.add('hover');
+            currHoveredItem.classList.remove('hover');
+        }
+    }
+    pressedEnterKey() {
+        const currHoveredItem = this.domContainer.getHoveredItem();
+        if(!currHoveredItem) {
+            this.launchSearchEvent();
+            return;
+        }
+        this.putSelectedItemToField(currHoveredItem.dataset.name);
+    }
+    changeHoveredItem(item) {
+        const currHoveredItem = this.domContainer.getHoveredItem();
+		if(currHoveredItem) {
+			currHoveredItem.classList.remove('hover');
+		}
+		item.classList.add('hover');
+    }
+    putSelectedItemToField(word) {
+        const searchField = this.domContainer.searchField;
+        searchField.value = word;
+
+        this.domContainer.autoCompleteList.innerHTML = '';
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+	const baseURL = "http://crong.codesquad.kr:8080/ac/";
+    const domContainer = new DomContainer();
+    const acResource = new ACResource();
+    const acRenderer = new ACRenderer(domContainer);
+    const acResponder = new ACResponder(domContainer, acResource, acRenderer, baseURL);
+});
+
+
+/***/ })
+/******/ ]);
